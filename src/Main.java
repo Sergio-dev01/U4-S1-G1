1644160abb3eb8860e1510b8c345ee5a557e3665
Sergio-dev01 <sergio.develoop@gmail.com>
2025-06-16 17:39:07 +0200

    U4-S1-G1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +11,18 @@ public class Main {
         return s + n;
     }
 
+    public static String[] inserisciInArray(String[] arr1, String str) {
+        String [] arr2 = new String[6];
+        arr2 [0] = arr1[0];
+        arr2 [1] = arr1[1];
+        arr2 [2] = str;
+        arr2 [3] = arr1[2];
+        arr2 [4] = arr1[3];
+        arr2 [5] = arr1[4];
+
+        return arr2;
+    }
+
     public static void main(String[] args) {
 
         int prodotto = moltiplica(3, 4);
@@ -17,6 +31,9 @@ public class Main {
         String RisultatoConcatena = concatena("Numero: ", 4);
         System.out.println("Risultato prodotto: " + RisultatoConcatena);
 
+        String[] arr = {"Uno", "Due", "Tre", "Quattro", "Cinque"};
+        String [] arrayResult = inserisciInArray(arr, "Pippo");
+        System.out.println(Arrays.toString(arrayResult));
 
     }
 }
